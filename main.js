81358ca571eb2d58857d83c2007c28862a2e2976
SwastiAK <85284257+SwastiAK@users.noreply.github.com>
2021-12-20 15:55:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 }
 
 console.log('ml5 version:',ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3zM_ewh8a/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aVPLuFcZO/model.json', modelLoaded);
 
 function modelLoaded()
 {
@@ -32,4 +32,36 @@ var synth = window.speechSynthesis;
 speak_data1 = "The first prediction is"+prediction_1;
 var utterThis = new SpeechSynthesisUtterance(speak_data1, speak_data2);
 synth.speak(utterThis);
+}
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+        prediction = results[0].label;
+        speak();
+
+        if(results[0].label=="Peace")
+        {
+            document.getElementById("result_emoji").innerHTML="&#9996;";
+        }
+        if(results[0].label=="Punch")
+        {
+            document.getElementById("result_emoji").innerHTML="&#9994;";
+        }
+        if(results[0].label=="Swag")
+        {
+            document.getElementById("result_emoji").innerHTML="&#129304;";
+        }
+    }
 }
